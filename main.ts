7ce065b30d2dec4d832187000ed77d38b7ae6604
mtornblad <mattias.tornblad@gmail.com>
2022-01-03 14:11:32 +0100

    Uppdatera pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,34 @@
-basic.forever(function () {
-	
+bluetooth.onBluetoothConnected(function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
 })
+bluetooth.onBluetoothDisconnected(function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+})
+control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+        RingbitCar.forward()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
+        RingbitCar.brake()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
+        RingbitCar.back()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
+        RingbitCar.brake()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
+        RingbitCar.turnleft()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP) {
+        RingbitCar.brake()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
+        RingbitCar.turnright()
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_UP) {
+        RingbitCar.brake()
+    } else {
+        serial.writeString("")
+    }
+})
+let strip: neopixel.Strip = null
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Red))
+bluetooth.startUartService()
+serial.redirectToUSB()
+serial.setBaudRate(BaudRate.BaudRate115200)
